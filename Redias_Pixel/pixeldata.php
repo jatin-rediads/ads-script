c74f9f113455b2fdcc66eeea56e9e7b56026509b
Ads Innov <jatinyva@gmail.com>
2022-10-24 14:26:40 +0530

    Pixel Added

--- Redias_Pixel/pixeldata.php
+++ Redias_Pixel/pixeldata.php
@@ -27,18 +27,10 @@
   echo '<br>';
   echo 'Protocol version: '.$mysqli->protocol_version;
 
-
-
-
-$fetchuid = "select ID from `pixel` where userid='".$_request['userid']."'";
-echo $fetchuid;
-$result = $mysqli -> query($fetchuid);
-
-if ($result->num_rows <= 0) {
     $insert_sql = "insert into `pixel`( `userid`, `domain`, `event`, `custom_event`, `version`, `location`, `referrer_location`, `timestamp_ms`, `encoding`, `screen_resolution`, `viewport`, `colordepth`, `title`, `browser`, `mobile_device`, `user_agent`, `timezone`,`meta_description`,`meta_keyword`,`country`,`city`,`region`,`isp`,`isp_type`,`ip`, `utm_source`, `utm_medium`, `utm_term`, `utm_campaign`, `utm_content`) values ('".$_request['userid']."','".$_request['url']."','".$_request['event']."','".$_request['ed']."','".$_request['pixel_version']."','".$_request['location_url']."','".$_request['referrer_url']."','".$_request['timestamp']."','".$_request['encoding']."','".$_request['screen_resolution']."','".$_request['viewport']."','".$_request['color_depth:']."','".$_request['title']."','".$_request['browser']."','".$_request['mobile']."','".$_request['useragent']."','".$_request['timezone']."','".$_request['metacontent']."','".$_request['metakeywords']."','".$_request['country']."','".$_request['city']."','".$_request['region']."','".$_request['isp']."','".$_request['ip']."','".$_request['gi']."','".$_request['utm_source']."','".$_request['utm_medium']."','".$_request['utm_term']."','".$_request['utm_campaign']."','".$_request['utm_content']."')";
     echo $insert_sql;
     $mysqli -> query($insert_sql);
-}
+
 
 // if($_request['ev'] != "pageload" && $_request['ev'] != "pageclose"){
 //     $insert_sql_meta = "insert into `pixel`( `userid`, `location`, `meta_key`, `meta_value`) values ('".$_request['userid']."','".$_request['url']."','".$_request['event']."','".$_request['ed']."')";
