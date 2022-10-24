c40ccc87e2bac7d3c207020bb1129bff01dbeb6e
Ads Innov <jatinyva@gmail.com>
2022-10-24 18:23:57 +0530

    Pixel Added

--- Redias_Pixel/pixeldata.php
+++ Redias_Pixel/pixeldata.php
@@ -1,6 +1,5 @@
 <?php
 
-
   $db_host = 'localhost';
   $db_user = 'adsinnov_pixel';
   $db_password = 'lO7sDAeHY4_B';
@@ -60,27 +59,15 @@ $utm_campaign = $_GET['utm_campaign'];
 $utm_term = $_GET['utm_term'];
 $utm_content = $_GET['utm_content'];
 
-
-
-
-
-
-echo $userid;
-
+//insert data into database
 $fetchuid = "select ID from `pixel` where userid='$userid'";
-echo $fetchuid;
 $result = $mysqli -> query($fetchuid);
 
 if ($result->num_rows <= 0) {
-    //$insert_sql = "insert into `pixel`( `userid`, `domain`, `event`, `custom_event`, `version`, `location`, `referrer_location`, `timestamp_ms`, `encoding`, `screen_resolution`, `viewport`, `colordepth`, `title`, `browser`, `mobile_device`, `user_agent`, `timezone`,`meta_description`,`meta_keyword`,`country`,`city`,`region`,`isp`,`isp_type`,`ip`, `utm_source`, `utm_medium`, `utm_term`, `utm_campaign`, `utm_content`) values ('".$_request['userid']."','".$_request['url']."','".$_request['event']."','".$_request['ed']."','".$_request['pixel_version']."','".$_request['location_url']."','".$_request['referrer_url']."','".$_request['timestamp']."','".$_request['encoding']."','".$_request['screen_resolution']."','".$_request['viewport']."','".$_request['color_depth:']."','".$_request['title']."','".$_request['browser']."','".$_request['mobile']."','".$_request['useragent']."','".$_request['timezone']."','".$_request['metacontent']."','".$_request['metakeywords']."','".$_request['country']."','".$_request['city']."','".$_request['region']."','".$_request['isp']."','".$_request['ip']."','".$_request['gi']."','".$_request['utm_source']."','".$_request['utm_medium']."','".$_request['utm_term']."','".$_request['utm_campaign']."','".$_request['utm_content']."')";
     $insert_sql = "insert into `pixel`( `userid`, `domain`, `event`, `custom_event`, `version`, `location`, `referrer_location`, `timestamp_ms`, `encoding`, `screen_resolution`, `viewport`, `colordepth`, `title`, `browser`, `mobile_device`, `user_agent`, `timezone`,`meta_description`,`meta_keyword`,`country`,`city`,`region`,`isp`,`isp_type`,`ip`, `utm_source`, `utm_medium`, `utm_term`, `utm_campaign`, `utm_content`) values ('".$userid."','".$domain."','".$event."','".$custom_event."','".$pixel_version."','".$location_url."','".$referrer_url."','".$timestamp."','".$encoding."','".$screen_resolution."','".$viewport."','".$color_depth."','".$title."','".$browser."','".$mobile."','".$useragent."','".$timezone."','".$metacontent."','".$metakeywords."','".$country."','".$city."','".$region."','".$isp."','".$ip."','".$gi."','".$utm_source."','".$utm_medium."','".$utm_term."','".$utm_campaign."','".$utm_content."')";
     echo $insert_sql;
     $mysqli -> query($insert_sql);
 }
 
-// if($_request['ev'] != "pageload" && $_request['ev'] != "pageclose"){
-//     $insert_sql_meta = "insert into `pixel`( `userid`, `location`, `meta_key`, `meta_value`) values ('".$_request['userid']."','".$_request['url']."','".$_request['event']."','".$_request['ed']."')";
-//     $mysqli -> query($insert_sql_meta);
-// }
   $mysqli->close();
 ?>
