318bf611b7eb1182f24387ef903e3e7457d38be8
Ads Innov <jatinyva@gmail.com>
2022-10-24 10:13:09 +0530

    Pixel Added

--- Redias_Pixel/pixeldata.php
+++ Redias_Pixel/pixeldata.php
@@ -1,16 +1,46 @@
 <?php
 
-$fetchuid = "SELECT id FROM `chitarkatha` WHERE chitarkatha_uid='".$_REQUEST['uid']."'";
+
+  $db_host = 'localhost';
+  $db_user = 'adsinnov_pixel';
+  $db_password = 'lO7sDAeHY4_B';
+  $db_db = 'adsinnov_pixel';
+  $db_port = 8889;
+
+  $mysqli = new mysqli(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db
+  );
+
+  if ($mysqli->connect_error) {
+    echo 'Errno: '.$mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: '.$mysqli->connect_error;
+    exit();
+  }
+
+  echo 'Success: A proper connection to MySQL was made.';
+  echo '<br>';
+  echo 'Host information: '.$mysqli->host_info;
+  echo '<br>';
+  echo 'Protocol version: '.$mysqli->protocol_version;
+
+  $mysqli->close();
+
+
+$fetchuid = "select id from `pixel` where userid='".$_request['userid']."'";
 $result = $mysqli -> query($fetchuid);
 
 if ($result->num_rows <= 0) {
-    $insert_sql = "INSERT INTO `chitarkatha`( `chitarkatha_uid`, `domain`, `event`, `custom_event`, `version`, `location`, `referrer_location`, `timestamp_ms`, `encoding`, `screen_resolution`, `viewport`, `colordepth`, `title`, `browser`, `mobile_device`, `user_agent`, `timezone`,`meta_description`,`meta_keyword`,`country`,`city`,`region`,`isp`,`isp_type`,`ip`, `utm_source`, `utm_medium`, `utm_term`, `utm_campaign`, `utm_content`) VALUES ('".$_REQUEST['uid']."','".$_REQUEST['id']."','".$_REQUEST['ev']."','".$_REQUEST['ed']."','".$_REQUEST['v']."','".$_REQUEST['dl']."','".$_REQUEST['rl']."','".$_REQUEST['ts']."','".$_REQUEST['de']."','".$_REQUEST['sr']."','".$_REQUEST['vp']."','".$_REQUEST['cd']."','".$_REQUEST['dt']."','".$_REQUEST['bn']."','".$_REQUEST['md']."','".$_REQUEST['ua']."','".$_REQUEST['tz']."','".$_REQUEST['tt']."','".$_REQUEST['mk']."','".$_REQUEST['gy']."','".$_REQUEST['cy']."','".$_REQUEST['rn']."','".$_REQUEST['isp']."','".$_REQUEST['ipt']."','".$_REQUEST['gi']."','".$_REQUEST['utm_source']."','".$_REQUEST['utm_medium']."','".$_REQUEST['utm_term']."','".$_REQUEST['utm_campaign']."','".$_REQUEST['utm_content']."')";
+    $insert_sql = "insert into `pixel`( `userid`, `domain`, `event`, `custom_event`, `version`, `location`, `referrer_location`, `timestamp_ms`, `encoding`, `screen_resolution`, `viewport`, `colordepth`, `title`, `browser`, `mobile_device`, `user_agent`, `timezone`,`meta_description`,`meta_keyword`,`country`,`city`,`region`,`isp`,`isp_type`,`ip`, `utm_source`, `utm_medium`, `utm_term`, `utm_campaign`, `utm_content`) values ('".$_request['userid']."','".$_request['url']."','".$_request['event']."','".$_request['ed']."','".$_request['pixel_version']."','".$_request['location_url']."','".$_request['referrer_url']."','".$_request['timestamp']."','".$_request['encoding']."','".$_request['screen_resolution']."','".$_request['viewport']."','".$_request['color_depth:']."','".$_request['title']."','".$_request['browser']."','".$_request['mobile']."','".$_request['useragent']."','".$_request['timezone']."','".$_request['metacontent']."','".$_request['metakeywords']."','".$_request['country']."','".$_request['city']."','".$_request['region']."','".$_request['isp']."','".$_request['ip']."','".$_request['gi']."','".$_request['utm_source']."','".$_request['utm_medium']."','".$_request['utm_term']."','".$_request['utm_campaign']."','".$_request['utm_content']."')";
     $mysqli -> query($insert_sql);
 }
 
-if($_REQUEST['ev'] != "pageload" && $_REQUEST['ev'] != "pageclose"){
-    $insert_sql_meta = "INSERT INTO `chitarkatha_meta`( `chitarkatha_uid`, `location`, `meta_key`, `meta_value`) VALUES ('".$_REQUEST['uid']."','".$_REQUEST['dl']."','".$_REQUEST['ev']."','".$_REQUEST['ed']."')";
-    $mysqli -> query($insert_sql_meta);
-}
+// if($_request['ev'] != "pageload" && $_request['ev'] != "pageclose"){
+//     $insert_sql_meta = "insert into `pixel`( `userid`, `location`, `meta_key`, `meta_value`) values ('".$_request['userid']."','".$_request['url']."','".$_request['event']."','".$_request['ed']."')";
+//     $mysqli -> query($insert_sql_meta);
+// }
 
 ?>
